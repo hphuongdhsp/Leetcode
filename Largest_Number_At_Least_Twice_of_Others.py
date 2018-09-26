48613c729e179967b4886068882f909bd9aed09b
Nguyen Hoang Phuong <37414457+hphuongdhsp@users.noreply.github.com>
2018-09-26 18:36:10 +0200

    Create Largest_Number_At_Least_Twice_of_Others.py

---
+++ Largest_Number_At_Least_Twice_of_Others.py
@@ -0,0 +1,45 @@
+"""
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+
+Note:
+
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
+"""
+
+import numpy as np
+class Solution(object):
+    def dominantIndex(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums=np.array(nums)
+        max=nums.max()
+        index=nums.argmax()
+        num=np.delete(nums,[index])
+        max_new=num.max()
+        if (max-2*max_new)>=0:
+            return index
+        else:
+            return -1
+        
